6855533d00a72bd64150eb94497e8dd8e645803d
Avuzwa Mdunyelwa <156984914+AvuMdunyelwa@users.noreply.github.com>
2026-06-11 17:47:55 +0000

    update profile page to display total reviews and likes for user

--- app.py
+++ app.py
@@ -230,7 +230,7 @@ def profile():
     message = request.args.get('message')
     user_id = session["user_id"]
     username = db_execute("SELECT username FROM users WHERE id=%(id)s", id=user_id)
-    reviews = db_execute("SELECT reviews.id, reviews.review_content, reviews.rating, reviews.cover_img_url, reviews.song_title, reviews.artist, COUNT(likes.id) AS total_likes FROM reviews LEFT JOIN likes ON likes.review_id = reviews.id WHERE reviews.user_id=%(user_id)s GROUP BY reviews.id, reviews.review_content, reviews.rating, reviews.cover_img_url, reviews.song_title, reviews.artist", user_id=user_id)
+    reviews = db_execute("SELECT reviews.id, reviews.review_content, reviews.rating, reviews.cover_img_url, reviews.song_title, reviews.artist, COUNT(likes.id) AS total_likes, (SELECT COUNT(*) FROM likes JOIN reviews r ON likes.review_id = r.id WHERE r.user_id = %(user_id)s) AS total_likes_received, (SELECT COUNT(*) FROM reviews WHERE user_id = 1) AS total_reviews FROM reviews LEFT JOIN likes ON likes.review_id = reviews.id WHERE reviews.user_id = 1 GROUP BY reviews.id, reviews.review_content, reviews.rating, reviews.cover_img_url, reviews.song_title, reviews.artist", user_id=user_id)
     return render_template("profile.html", username=username[0]['username'], reviews=reviews, message=message)
 
 
